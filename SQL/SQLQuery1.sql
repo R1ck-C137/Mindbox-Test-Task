85d87e999471721df7325fca7113116bfba63899
R1ck-C137 <61411463+R1ck-C137@users.noreply.github.com>
2022-09-07 17:38:26 +0300

    Update SQLQuery1.sql

--- SQL/SQLQuery1.sql
+++ SQL/SQLQuery1.sql
@@ -10,7 +10,7 @@ create table Categories
 Id bigint identity(1,1) not null primary key,
 NameCategory varchar(50) not null
 )
-select prod.NameProduct [Имя продукта], cat.NameCategory [Имя категории]
+select prod.NameProduct [Name Product], cat.NameCategory [Name Category]
 from Products prod
 join Categories cat on prod.IdCategory = cat.id
 order by cat.NameCategory
